ebe260e35b2d16503a824083f3324d3b6a861504
Haquangnguyen30 <haquangnguyen30@gmail.com>
2025-03-18 22:45:03 +0700

    Sua file serializer cua department

--- department/serializers.py
+++ department/serializers.py
@@ -5,8 +5,11 @@ class DepartmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Department
         fields = '__all__'
+        extra_kwargs = {
+            'managerID': {'allow_null': True, 'required': False}  # Cho phép managerID null
+        }
 
     def validate_departmentName(self, value):
-        if not value.strip():  # Kiểm tra nếu tên phòng ban bị rỗng hoặc chỉ chứa khoảng trắng
+        if not value.strip():
             raise serializers.ValidationError("Tên phòng ban không được để trống.")
         return value
