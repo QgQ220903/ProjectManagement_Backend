ddf55c23d88958e8e7722162fa388db8d34232e2
QuachQuy <quyquach22092003@gmail.com>
2025-03-20 23:41:30 +0700

    QGQ fix task-assignment

--- task_assignment/serializers.py
+++ task_assignment/serializers.py
@@ -3,11 +3,19 @@ from rest_framework import serializers
 from .models import TaskAssignment
 from employee.serializers import EmployeeSerializer  # Import EmployeeSerializer
 from task.serializers import TaskSerializer # Import TaskSerializer
+from employee.models import Employee
+from task.models import Task
 
 class TaskAssignmentSerializer(serializers.ModelSerializer):
-    employee = EmployeeSerializer(read_only=True)  # Sử dụng EmployeeSerializer
-    task = TaskSerializer(read_only=True)
+    employee = serializers.PrimaryKeyRelatedField(queryset=Employee.objects.all())
+    task = serializers.PrimaryKeyRelatedField(queryset=Task.objects.all())
 
     class Meta:
         model = TaskAssignment
         fields = ['id', 'employee', 'task', 'role', 'status']
+    
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['employee'] = EmployeeSerializer(instance.employee).data
+        representation['task'] = TaskSerializer(instance.task).data
+        return representation
