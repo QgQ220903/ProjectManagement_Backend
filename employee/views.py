f54df9a885d5e98289b0e69cab7be36599d798f7
Tran Truong <chantruong753@gmail.com>
2025-03-27 20:30:31 +0700

    pagination employee

--- employee/views.py
+++ employee/views.py
@@ -4,10 +4,15 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Employee
 from .serializers import EmployeeSerializer
+from rest_framework.pagination import PageNumberPagination
+
+# class EmployeePagination(PageNumberPagination):
+#     page_size = 5  # Số lượng phần tử trên mỗi trang
 
 class EmployeeListCreate(generics.ListCreateAPIView):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
+    # pagination_class = EmployeePagination  # Thêm dòng này
 
 class EmployeeRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = Employee.objects.all()
