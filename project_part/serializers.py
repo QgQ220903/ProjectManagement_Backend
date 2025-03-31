c0b86b08dee9a5cfc7ae943908d08307589ffde3
cửu phan duy <0cauchin0@gmail.com>
2025-03-31 21:12:48 +0700

    fix projectpart-department

--- project_part/serializers.py
+++ project_part/serializers.py
@@ -1,13 +1,14 @@
 # project_part/serializers.py
 from rest_framework import serializers
 from .models import ProjectPart
+from department.serializers import DepartmentSerializer
 from task.task_detail_serializers import TaskDetailSerializer
 from department.models import Department
 
 class ProjectPartSerializer(serializers.ModelSerializer):
     tasks = serializers.SerializerMethodField()
-    department = serializers.PrimaryKeyRelatedField(queryset=Department.objects.all(), required=False)  # Chỉnh sửa để trường department không bắt buộc
-
+    department = DepartmentSerializer(read_only=True)  # Hiển thị chi tiết department khi GET
+    department_id = serializers.PrimaryKeyRelatedField(queryset=Department.objects.all(), write_only=True)
     class Meta:
         model = ProjectPart
         fields = '__all__'
@@ -17,3 +18,7 @@ class ProjectPartSerializer(serializers.ModelSerializer):
         tasks = obj.tasks.filter(is_deleted=False, parent_task__isnull=True).order_by('-created_at')
         serializer = TaskDetailSerializer(tasks, many=True)
         return serializer.data
+    def create(self, validated_data):
+        department = validated_data.pop('department_id', None)  # Lấy ID của department từ dữ liệu gửi lên
+        project_part = ProjectPart.objects.create(**validated_data, department=department)  # Tạo bản ghi với department đúng kiểu
+        return project_part
